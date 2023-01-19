1dee4febefc22a56ef963a8d3e20619297b4f319
Muthu <muthu98wic@gmail.com>
2023-01-19 12:56:29 +0530

    2nd

--- my-app/src/components/main/main.js
+++ my-app/src/components/main/main.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Bottom } from "./bottom/bottom";
 import { Card } from "./card/card";
 import { Graph } from "./graph/graph";
-import icon1 from "../../images/cardicon1.png";
-import icon2 from "../../images/cardicon2.png";
-import icon3 from "../../images/cardicon3.png";
+
 import "../main/main.css";
 
 export const Main = () => {
